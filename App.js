e1d638928b9a6729ffbede14fd634cb9f185be80
li2he1 <peterli2he1@gmail.com>
2021-11-27 21:33:54 -0800

    part1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -11,6 +11,9 @@ export default function App() {
         <Text style = {styles.sectionTitle}>Today's Tasks</Text>
         <View style = {styles.items}>
           {/* This is where the tasks will go! */}
+          <Task text ={'Task1'}/>
+          <Task text ={'Task2'}/>
+
         </View>
       </View>
     </View>
@@ -30,5 +33,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight:'bold'
   },
-  items: {},
+  items: {
+    marginTop:30,
+  },
 });
